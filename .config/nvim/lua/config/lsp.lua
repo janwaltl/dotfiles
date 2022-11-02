5de46b8890f8f78d0d9ea018d9a88b6850acce8f
Jan Waltl <waltl.jan@gmail.com>
2022-11-02 23:21:01 +0100

    chore: replace deprecated call

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -61,7 +61,7 @@ end
 
 lsp_installer.on_server_ready(function(server)
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+	capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 	local opts = {
 		on_attach = common_on_attach,
 		capabilities = capabilities,
