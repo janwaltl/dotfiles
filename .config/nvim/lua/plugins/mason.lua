c9595fbd66c5faa1ff23b7b70d5a0653e8e742d6
Jan Waltl <waltl.jan@gmail.com>
2025-06-22 22:04:54 +0200

    chore: autoinstall Mason tools

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,8 +1,35 @@
+local ensure_installed = {
+	-- Rust
+	"rust_analyzer",
+	"ruff",
+	"basedpyright",
+	-- Lua
+	"lua_ls",
+	"stylua",
+	-- C++
+	"clangd",
+	-- YAML,
+	"yamlfmt",
+	"yamllint",
+	-- Misc,
+	"codespell",
+}
+
 return {
 	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
+
+			local registry = require("mason-registry")
+			for _, tool in ipairs(ensure_installed) do
+				if registry.has_package(tool) then
+					local t = registry.get_package(tool)
+					if not t:is_installed() then
+						t:install()
+					end
+				end
+			end
 		end,
 	},
 	{
@@ -12,19 +39,22 @@ return {
 		},
 		config = function()
 			local mason_lspconfig = require("mason-lspconfig")
+			local registry = require("mason-registry")
 
+			-- Collect all LSP servers
+			local lsps = {}
+			for _, tool in ipairs(ensure_installed) do
+				if registry.has_package(tool) then
+					local t = registry.get_package(tool)
+					for _, value in ipairs(t.spec.categories) do
+						if value == "LSP" then
+							table.insert(lsps, tool)
+						end
+					end
+				end
+			end
 			mason_lspconfig.setup({
-				ensure_installed = {
-					-- Rust
-					"rust_analyzer",
-					"ruff",
-					-- Python
-					"basedpyright",
-					-- Lua
-					"lua_ls",
-					-- C++
-					"clangd",
-				},
+				ensure_installed = lsps
 			})
 		end,
 	},
