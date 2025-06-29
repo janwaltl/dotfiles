b2dddd4ca18d718b825962e6d83bfeb0ce283909
Jan Waltl <waltl.jan@gmail.com>
2025-06-29 22:08:15 +0200

    chore: debug logs

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -9,4 +9,32 @@ function M.kmap(mode, lhs, rhs, opts)
 	vim.keymap.set(mode, lhs, rhs, options)
 end
 
+local function script_path()
+	return debug.getinfo(2, "S").source:sub(2)
+end
+
+M._log_path = '/tmp/nvimdebug.log'
+
+function M.log(...)
+	if not M._fh then
+		M._fh = io.open(M._log_path, 'a')
+	end
+	local buf = {}
+	for i = 1, select('#', ...) do
+		local v = select(i, ...)
+		table.insert(buf, vim.inspect(v))
+	end
+	M._fh:write(
+		os.date('%Y:%m:%d %H.%M.%S')
+		.. ' '
+		.. script_path()
+		.. '\n'
+		.. table.concat(buf, '\n')
+		.. '\n\n'
+	)
+	M._fh:flush()
+end
+
+M.log("Hello there")
+
 return M
